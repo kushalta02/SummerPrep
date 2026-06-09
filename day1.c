0eac28541911876ab16e1828a8ed286423ce3117
Kushalta <kushalta162@gmail.com>
2026-06-09 23:10:40 +0530

    day1

---
+++ day1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    printf("Hello, World!\n");
+    int a = 5;int b = 10;
+    int sum = a + b;
+    int multiplication = a * b;
+    int division = b / a;
+    float division_float = (float)b / (float)a;
+    printf("The sum of %d and %d is %d\n", a, b, sum);
+    printf("The multiplication of %d and %d is %d\n", a, b, multiplication);
+    printf("The division of %d and %d is %d\n", a, b, division);
+    printf("The float division of %d and %d is %.2f\n", a, b, division_float);
+
+    return 0;
+}
